caceff07c5030fdb540f552acaf2707b91277be8
Frank Kevin Zey <frank.kevin.zey@mni.thm.de>
2012-06-14 14:32:36 +0200

    created FileFilter for jpeg files in JFileChooser

--- src/de/thm/ateam/memoryDeckLoader/io/ImageLoader.java
+++ src/de/thm/ateam/memoryDeckLoader/io/ImageLoader.java
@@ -11,6 +11,7 @@ package de.thm.ateam.memoryDeckLoader.io;
 import java.io.File;
 
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 
 /**
  * ImageLoader class as singleton class to get image files
@@ -37,14 +38,23 @@ public class ImageLoader {
 	}
 	
 	/**
+	 * Opens an open dialog to select a file
 	 * 
-	 * 
-	 * @return
+	 * @return File Returns a selected file, otherwise null
 	 */
 	public File getImageWithUI() {
 		JFileChooser fc = new JFileChooser();
 		File f = null;
 		
+		fc.setFileFilter(new FileFilter() {
+			
+			public boolean accept(File f) {
+                return f.getName().toLowerCase().endsWith(".jpeg") || f.isDirectory();
+            }
+            public String getDescription() {
+                return "*.jpeg";
+            }
+		});
 		fc.setAccessory(new ImagePreview(fc));
 		if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
 			f = fc.getSelectedFile();
